a21c5d1a8cf7c1124635f2e57406130229fa082b
nefera606 <garciademata@gmail.com>
2018-04-13 10:04:25 +0200

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -10,5 +10,16 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     }
+    //Dont forget adding alastria network
   }
 };
+/* Example:
+'alastra': {
+  host: "alastrianodeip",
+  port: 22000,
+  network_id: "*",
+  gas: 0xfffff,
+  gasPrice: 0x0,
+  from: "yourAccount"
+}
+*/
